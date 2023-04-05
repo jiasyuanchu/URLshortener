fa3c951e789b5f51c81b0252fb5ce58c0f324957
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-05 20:27:08 +0800

    feat: project init

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+  res.send("hello world!")
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
