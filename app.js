d9eb1a0d6ae71b5761d9eac333940886b3d9ac5c
jiasyuanchu <jiasyuanchu@gmail.com>
2023-04-05 20:45:42 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send("hello world!")
+  res.render('index')
 })
 
 app.listen(port, () => {
